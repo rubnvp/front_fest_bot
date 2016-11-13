4fe5ed2644f7ec3d600424f9adfa6a46b263d065
Ruben <rubn.vp@gmail.com>
2016-11-13 18:06:57 +0100

    remove echo command

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var TelegramBot = require('node-telegram-bot-api');
-var http = require('http');
 
 var TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 if (TELEGRAM_TOKEN === undefined) {
@@ -11,20 +10,24 @@ if (TELEGRAM_TOKEN === undefined) {
 var bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
 
 // Matches /echo [whatever]
-bot.onText(/\/start/, function (msg) {
+bot.onText(/^\/start$/, function (msg) {
   var fromId = msg.from.id;
-  bot.sendMessage(fromId, "Hola, soy el bot del frontFest! Estaré encantado de responder tus preguntas sobre el evento");
+  bot.sendMessage(fromId, "Hola, soy el bot del frontFest! Estaré encantado de responder tus preguntas sobre el evento.");
 });
 
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
+//Ask for event location
+bot.onText(/^\/location$/, function (msg) {
+  var chatId = msg.chat.id;
+  latitude = '40.412664';
+  longitude = '-3.718301';
+  title = 'frontFest (Google Campus)';
+  address = 'Calle Moreno Nieto, 2, 28005 Madrid, Spain';
+  bot.sendVenue(chatId, latitude, longitude, title, address);
+  bot.sendMessage(chatId, 'Se accede por la entrada del auditorio.');
 });
 
 // Matches /cat
-bot.onText(/\/cat/, function (msg) {
+bot.onText(/^\/cat$/, function (msg) {
   var chatId = msg.chat.id;
   var width = getRandomInt(100, 500);
   var height = getRandomInt(100, 500);
@@ -36,18 +39,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//Ask for event location
-bot.onText(/\/ubicacion/, function (msg) {
-  var chatId = msg.chat.id;
-  latitude = '40.412664';
-  longitude = '-3.718301';
-  title = 'frontFest (Google Campus)';
-  address = 'Calle Moreno Nieto, 2, 28005 Madrid, Spain';
-  bot.sendVenue(chatId, latitude, longitude, title, address);
-  bot.sendMessage(chatId, 'Se accede por la entrada del auditorio');
-});
-
 // server to keep alive on heroku
+var http = require('http');
 var server = http.createServer(function(request, response) {
   response.end("Hi there! :)");
 });
